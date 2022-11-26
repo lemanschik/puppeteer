91aa0db93146ebd3fe71c987569bf30b50484c5a
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-26 07:21:50 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,20 @@
+/** 
+ * Design fundamentals:
+ * It exists only the ESM Format if some one needs CJS
+ * he can produce that mostly in under 1 Secund on a slow machine
+ * running a single command. 
+ * This also is designed without 3th party dependencies eventEmitter
+ * got replaced by ReadableStream WriteableStream.
+ *
+ * we take the broken puppeteer ESM build and reuse it to avoid messing
+ * with the broken typescript build. They run tests on it till it produces
+ * something and we take that something and call it a day as also apply 
+ * ESM Patches to it to make ti real ESM like we uptranspile the code again. 
+ * Also we try to emit correct types from the resulting ESM Code directly.
+ * as every const and class is already a inferable type by design. 
+ * Real ESM is lexical Scoped and Static Analyzeable by default. 
+ */
+
 const rollupConfig = { 
   output: { format: 'esm', dir: '.' },
   plugins: [{ buildStart() {
