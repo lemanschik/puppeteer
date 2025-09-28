489542d35b8070ff3279cb46c46f47ce47d9c127
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-09-28 11:55:20 +0200

    Create ws.js
    HotFix ESM ws

---
+++ packages/puppeteer-core/third_party/ws/ws.js
@@ -0,0 +1,12 @@
+// TODO: remove this whole package once https://github.com/websockets/ws/pull/2299 landed.
+// we get wrapper.mjs which has nested defaults
+import * as wrapperWebSocket from 'cjs-ws';
+
+const createWebSocketStream = wrapperWebSocket.createWebSocketStream.default;
+const Receiver = wrapperWebSocket.Receiver.default;
+const Sender = wrapperWebSocket.Sender.default;
+const WebSocket = wrapperWebSocket.WebSocket.default;
+const WebSocketServer = wrapperWebSocket.WebSocketServer.default;
+
+export { createWebSocketStream, Receiver, Sender, WebSocket, WebSocketServer };
+export default WebSocket;
